0bf0c9123dc9d25e93021f64c2b060d6ec171147
Matt J <matt.jellings@gmail.com>
2018-10-09 16:05:20 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <p>Welcome to my Trello clone, I have created this as a way to learn Laravel.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
