8638decc1288e81bfa0598333065cf27c6b0f386
Matt J <matt.jellings@gmail.com>
2018-10-15 13:55:37 +0100

    Added migration to git push hook

--- public/tools/git.hook.push.php
+++ public/tools/git.hook.push.php
@@ -3,6 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 //if ( $_POST['payload'] ) {
   shell_exec( 'cd c:\xampp\htdocs\trello.jello.me.uk && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd c:\xampp\htdocs\trello.jello.me.uk && php artisan migrate' );
 //}
 ?>Test 6
 
