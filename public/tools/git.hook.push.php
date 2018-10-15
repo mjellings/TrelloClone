615816ba6f02f2b22a4d5445c850815793e0b546
Matt J <matt.jellings@gmail.com>
2018-10-15 12:32:18 +0100

    Updated push hook #4

--- public/tools/git.hook.push.php
+++ public/tools/git.hook.push.php
@@ -1,8 +1,8 @@
 <?php
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] ) {
+//if ( $_POST['payload'] ) {
   shell_exec( 'cd c:\xampp\htdocs\trello.jello.me.uk && git reset --hard HEAD && git pull' );
-}
-?>Test 2
+//}
+?>Test 3
 
