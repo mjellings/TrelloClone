01f645474508a3299c8fb810182200d5402de2f3
Matt J <matt.jellings@gmail.com>
2018-10-22 11:18:11 +0100

    WIP - Edit cards form and functionality

--- resources/views/cards/edit.blade.php
+++ resources/views/cards/edit.blade.php
@@ -25,7 +25,26 @@
                                         </div>
                                     @endif
 
-                                    Form here
+                                    <form action="/cards/{{ $card->id }}/edit" method="post">
+                                    {{ csrf_field() }}
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label>Title</label>
+                                                    <input type="text" class="form-control border-input" name="title" value="{{ $card->title }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label>Content</label>
+                                                    <textarea class="form-control border-input" name="content">{{ $card->content }}</textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-info btn-fill btn-wd">Update Card</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
