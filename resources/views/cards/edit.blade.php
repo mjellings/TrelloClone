9d4d748490c89080bdc158c1f22ccbb2df88f72e
Matt J <matt.jellings@gmail.com>
2018-10-22 12:10:27 +0100

    WIP : Edit card, functionality complete, adding the ability to link tags

--- resources/views/cards/edit.blade.php
+++ resources/views/cards/edit.blade.php
@@ -1,51 +1,69 @@
 @extends('layouts.app', compact($boards))
 
 @section('content')
-<div class="content">
+        <div class="content">
             <div class="container-fluid">
-                
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="content">
+
+                                @if (count($errors) > 0)
+                                <!-- Form Error List -->
+                                <div class="alert alert-danger">
+                                    <strong>Whoops! Something went wrong!</strong>
+
+                                    <br><br>
+
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                @endif
+
+                                <form action="/cards/{{ $card->id }}/edit" method="post">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" name="card_id" value="{{ $card->id }}" />
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Title</label>
+                                                <input type="text" class="form-control border-input" name="title" value="{{ $card->title }}">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Content</label>
+                                                <textarea rows="5" class="form-control border-input" name="content">{{ $card->content }}</textarea>
+                                                <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">* Markdown supported</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-info btn-fill btn-wd">Update Card</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
                             <div class="header">
-                                <div class="content">
-
-                                    @if (count($errors) > 0)
-                                        <!-- Form Error List -->
-                                        <div class="alert alert-danger">
-                                            <strong>Whoops! Something went wrong!</strong>
-
-                                            <br><br>
-
-                                            <ul>
-                                                @foreach ($errors->all() as $error)
-                                                    <li>{{ $error }}</li>
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                    @endif
-
-                                    <form action="/cards/{{ $card->id }}/edit" method="post">
-                                    {{ csrf_field() }}
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label>Title</label>
-                                                    <input type="text" class="form-control border-input" name="title" value="{{ $card->title }}">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label>Content</label>
-                                                    <textarea class="form-control border-input" name="content">{{ $card->content }}</textarea>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-info btn-fill btn-wd">Update Card</button>
-                                        </div>
-                                    </form>
-                                </div>
+                                <h4 class="title">Tags</h4>
+                            </div>
+                            <div class="content">
+                                @foreach ($tags as $tag)
+                                {{ Form::checkbox('selected_tags[]', $tag->id, in_array($tag->id, $current_tags)) }}
+                                {{ Form::label('selected_tag', $tag->label, array('class' => "card_tag card_" . $tag->class)) }}<br>
+                                @endforeach
                             </div>
                         </div>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@
 */
 
 use App\Card;
+use App\Board;
+use App\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 Route::get('/', function () {
     $boards = array();
@@ -28,13 +32,52 @@ Route::get('/cards/{id}/edit', function($id) {
     // Boards for the nav menu
     $boards = Auth::User()->boards()->orderBy('name', 'asc')->get();
 
+    // Retrieve full tags list
+    $tags = Tag::orderBy('label', 'asc')->get();
+
+    $current_tags = array();
+
+    // Find existing card
     $card = Card::find($id);
+
+    foreach ($card->tags as $tag) {
+        $current_tags[] = $tag->id;
+    }
+
     return view('cards.edit', [
         'boards' => $boards,
         'page_title' => 'Edit Card - ' . $card->title,
         'card' => $card,
+        'tags' => $tags,
+        'current_tags' => $current_tags
     ]);
 
+});
+
+Route::post('/cards/{id}/edit', function (Request $request) {
+    // Boards for the nav menu
+    $boards = Auth::User()->boards()->orderBy('name', 'asc')->get();
+
+    // Validation rules
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'content' => 'required',
+    ]);
+
+    // Redirect if validation fails
+    if ($validator->fails()) {
+        return redirect('/cards/' . $request->card_id . '/edit')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Find existing card
+    $card = Card::find($request->card_id);
+    $card->title = $request->title;
+    $card->content = $request->content;
+    $card->save();
+
+    return redirect('/boards/' . $card->board_id);
 });
 
 Auth::routes();
